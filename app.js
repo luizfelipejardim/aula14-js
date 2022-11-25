71977128ea8783bc5a58432c9227ed91b504d757
luizfelipejardim <luizfelipe.jardim@hotmail.com>
2022-11-25 20:13:11 -0300

    atualuzado

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const modulos = require("./modulos");
 
 //A partir dos 10 usuários, gereuma lista onde todos eles convergem (lista de objetos)
 
-// console.log(contas);
+console.log(modulos.contas);
 
 
 
@@ -12,8 +12,24 @@ const modulos = require("./modulos");
 //uma propriedade chamada clientes, ele será composto pela lista de objetos
 //gerados no ponto anterior.
 
+let banco = {
+    clientes: modulos.contas
+}
 
-console.log(modulos.banco(5))
+console.log(banco)
 
+
+
+//o objeto do banco criará um método chamado consultarCliente que receberá um
+//nome (titular) por parâmetro e deve pesquisar na lista de contas e retornar ao
+//objeto do cliente que corresponde a esse nome inserido
+
+
+function consultarClientes(nome){
+   return nome.titular === 'Jarret Lafuente'
+}
+
+consultarClientes('Jarret Lafuente')
+console.log(modulos.contas.find(consultarClientes));
 
 
